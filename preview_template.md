a53a7032f8df6400cdc91f0fba1c1b22f766b771
PyatiyEtaj <pyatiyEtaj@gmail.com>
2020-04-18 13:38:51 +0400

    render md with extensions, gen binaries from azure

---
+++ preview_template.md
@@ -0,0 +1,28 @@
+
+
+# ШАБЛОННЫЙ_ВИД
+Вот случайное число 59
+
+выр = $\arctan{(\tan{(\frac{\cos{(x1)}}{x1 * -19,211179259797177})})}$
+
+123
+
+код = if ( (fabs((x1 * -19,211179259797177)) > 1.0e-6)) y = atan(tan(cos(x1) / (x1 * -19,211179259797177))); else y = 0.0;
+# РЕШЕНИЕ
+```
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    system("pause");
+    return 0;
+}
+```
+# ТЕСТОВЫЕ_ДАННЫЕ
+```
+[{"Name":"тест1","Data":["#rnd(10 , 200 , int , 10)"]}]
+```
--- src/extension.ts
+++ src/extension.ts
@@ -1,85 +1,106 @@
 import * as vscode from 'vscode';
 import { exec } from 'child_process';
 import * as fs from 'fs';
+const request = require('request');
+const unzipper = require('unzipper'); 
+const fetch = require("node-fetch");
 
 export function activate(context: vscode.ExtensionContext) {
 	
-	const myCommandId = 'extension.previewTemplate';
-	let panel : any;
-	let panelIsCreated = false;
+	const previewTemplateId = 'extension.previewTemplate';
+	const updateGenId = 'extension.updateGen';
 	let needToCheckGenFolder = true;
-	let genBinariesPath = `${vscode.extensions.getExtension("pyatiyetaj.gen-template-viewer")?.extensionPath}\\generator_binaries`;
-	let generatorPath = `${genBinariesPath}\\Generator`;
-	const getGeneratorFromGit = async () =>{
-		if (!fs.existsSync(genBinariesPath))
-		{
-			vscode.window.showInformationMessage(`getting generator binaries...`);
-			await exec(`git clone https://github.com/PyatiyEtaj/vscode-generator-binaries ${genBinariesPath}`,
-			async (error, stdout, stderr) =>{
-				if (error)
-					vscode.window.showInformationMessage(`error: ${error.message}`);
-				else{
-					vscode.window.showInformationMessage(`done!`);
-					needToCheckGenFolder = false;
-				}
-			});
-		}		
+	let outputString = "NULL";
+	const myScheme = 'Previewer';	
+	const extensionPath = vscode.extensions.getExtension("pyatiyetaj.gen-template-viewer")?.extensionPath;
+	//const genBinariesPath = `${extensionPath}\\generator_binaries`;
+	const genBinariesPath = `${extensionPath}\\gen\\Generator`;
+	const generatorPath = `${genBinariesPath}\\Generator`;
+
+	const getGeneratorBinaries = () => {
+		vscode.window.showInformationMessage(`getting generator binaries...`)
+		fetch('https://dev.azure.com/pyatiyetaj/Generator/_apis/build/builds?api-version=5.0')
+			.then( (res : any) => res.json())
+			.then( (data : any) => {
+				const buildId = data.value.length+1        
+				fetch(`https://dev.azure.com/pyatiyetaj/Generator/_apis/build/builds/${buildId}/artifacts?api-version=5.0`)
+					.then( (res : any) => res.json())
+					.then( (data : any) => {
+						const url = data.value[0].resource.downloadUrl;
+						request(url).pipe(
+							unzipper.Extract({ path: `${extensionPath}` })
+						)
+						vscode.window.showInformationMessage(`done!`);
+					})
+			})
 	}
 
-	//TODO : update binaries
-	/*const pullGeneratorFromGit = async () =>{
-		if (fs.existsSync(genBinariesPath))
-		{
-		}
-	}*/
+	const updateGenerator = () => {if (!fs.existsSync(genBinariesPath)) getGeneratorBinaries();}
 
-	const previewTemplate = async () => {
+	const myProvider = new class implements vscode.TextDocumentContentProvider {
+
+		onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
+		onDidChange = this.onDidChangeEmitter.event;
+
+		provideTextDocumentContent(___: vscode.Uri): string {
+			return outputString;
+		}
+	}
+	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(myScheme, myProvider));
+
+	const previewTemplate = async (flag : boolean = true) => {		
 		let doc = vscode.window.activeTextEditor?.document;
 		if (doc && doc.languageId == "gentemp")
 		{
-			if (needToCheckGenFolder) getGeneratorFromGit();
+			if (needToCheckGenFolder) updateGenerator();
 			let path = (vscode.window.activeTextEditor?.document.uri.path ?? "");
-			if (process.platform === "win32" && path.length > 0) path = path.substr(1, path.length-1);
-			//vscode.window.showInformationMessage(vscode.extensions.getExtension("pyatiyetaj.gen-template-viewer")?.extensionPath ?? "ERROR");
-			let cmd = process.platform === "win32" ? `chcp 65001 | ${generatorPath}.exe \"${path}\"` : `${generatorPath} \"${path}\"`;
-
-			await exec(cmd, async (error, stdout, stderr) =>{
+			if (process.platform === "win32" && path.length > 0) path = path.substr(1, path.length-1);			
+			let cmd = process.platform === "win32" ? `chcp 65001 | ${generatorPath}.exe` : `${generatorPath}`; // тут с линуксом скорее всего проблемы будут
+			//let cmd = `chcp 65001 | dotnet ${generatorPath}.dll` <- есть право на жизнь, но нужно тестить
+			//let cmd = `chcp 65001 | Generator.exe \"${extensionPath}\" \"${path}\"`
+			await exec(`${cmd} \"${extensionPath}\" \"${path}\"`, async (error, stdout, stderr) =>{
 				if (error)
 					console.error(`error: ${error.message}`);
 				else if (stderr)
 					console.log(`stderr: ${stderr}`);
 				else
-					await openMarkdownPreviewSideBySide(stdout);
-					/*
-					// это с файлом
-					{
-						vscode.window.activeTextEditor?.document.fileName
-						let doc = await vscode.workspace.openTextDocument(
-							`${vscode.window.activeTextEditor?.document.fileName}.result_gen_template.txt`
-						); // calls back into the provider
-						await vscode.window.showTextDocument(doc, { preview: false, viewColumn : vscode.ViewColumn.Two  });
-					}*/
-					
+					await openMarkdownPreviewSideBySide(stdout, flag);					
 			});
 		}
 	};
 
-	const openMarkdownPreviewSideBySide = async (text : string) => {	
+	const openMarkdownPreviewSideBySide = async (text : string, flag : boolean) => {	
+		outputString = text;
+		const vfUri = vscode.Uri.parse(`${myScheme}:` + 'preview');
+		myProvider.onDidChangeEmitter.fire(vfUri);
+		if (flag) {
+			await vscode.commands.executeCommand('markdown.showPreviewToSide', vfUri);
+		}
+	}	
+	
+	let disposablePT = vscode.commands.registerCommand(previewTemplateId, previewTemplate);
+	let disposableUG = vscode.commands.registerCommand(updateGenId, getGeneratorBinaries);
+
+	vscode.workspace.onDidSaveTextDocument((e : vscode.TextDocument) => previewTemplate(false));
+
+	context.subscriptions.push(disposablePT);
+	context.subscriptions.push(disposableUG);
+}
+
+export function deactivate() {}
+
+
+/*
+const openMarkdownPreviewSideBySide = async (text : string) => {	
 		if (!panelIsCreated) {
-			panel = await vscode.window.createWebviewPanel('markdown-viewer', "Prewiev", vscode.ViewColumn.Two);	
+			panel = await vscode.window.createWebviewPanel('markdown.preview', "Prewiev", vscode.ViewColumn.Two);	
 			panelIsCreated = true;
 			panel.onDidDispose(() =>{panelIsCreated = false;})
 		}
 		await vscode.commands.executeCommand('markdown.api.render', text).then(result => {
-			panel.webview.html = " " + result;
-		})
+			panel.webview.html = result;
+		})		
 	}
+*/
 
-	
-	
-	let disposable = vscode.commands.registerCommand(myCommandId, previewTemplate);
-	vscode.workspace.onDidSaveTextDocument((e : vscode.TextDocument) => previewTemplate());
-	context.subscriptions.push(disposable);
-}
-
-export function deactivate() {}
+//        "begin": "(#rnd|#genAE|#getAEcode|#lua)\\(",
